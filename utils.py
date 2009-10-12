7cc606e560ff2b221219b18bd2a1e1788fecd3a9
Roman Kharitonov <refaim.vl@gmail.com>
2009-10-13 10:26:43 +1100

    More accurate tokenizer output.

--- utils.py
+++ utils.py
@@ -23,7 +23,7 @@ def print_token_array(tokens):
     space = " "
     indent_len = 2
     indent = indent_len * space
-    b_horz, b_vert = "-", "|"
+    b_horz, b_vert, b_cross = "-", "|", "+"
     
     colcount = 5
     headers = ["Line, pos", "Token text", "Token value", "Token type", "Error"]
@@ -45,31 +45,34 @@ def print_token_array(tokens):
         return [linepos_template.format(tok.line, tok.pos), tok.text, str(tok.value), lexems_str[tok.type], tok.errmsg]
     
     def print_line(out, row, col):
-        left, right = b_vert + indent, indent + b_vert
-        emptyline = left + max_lens[col] * space + right
+        left_border = b_vert if col == 0 else ""
+        left, right = left_border + indent, indent + b_vert
+        emptyline = left + max_value_lens[col] * space + right
+        
         if row in borders:
-            out[row] += b_horz * (max_lens[col] + 2 * (indent_len + len(b_vert)))
+            length = max_value_lens[col] + len(left + right)
+            right_border = b_cross if col == colcount - 1 else ""            
+            out[row] += b_cross + b_horz * (length - len(left_border + b_vert)) + right_border 
         elif row in empty_lines:
             out[row] += emptyline
         elif row == header:
-            a, b = max_lens[col], len(headers[col])
+            a, b = max_value_lens[col], len(headers[col])
             lshift = int((a - b) / 2) if a > b else 0
-            rshift = max_lens[col] - len(headers[col]) - lshift
+            rshift = max_value_lens[col] - len(headers[col]) - lshift
             out[row] += left + space * lshift + headers[col] + space * rshift + right
         else:
             tok = tokens[row - tokens_shift]
             value = values(tok)[col]
-            rshift = max_lens[col] - len(value)
+            rshift = max_value_lens[col] - len(value)
             out[row] += left + value + space * rshift + right
-            out[row]
 
-    max_lens = [0] * colcount
+    max_value_lens = [0] * colcount
 
     errors_present = False
     for tok in tokens:
         if tok.error: errors_present = True
         for i in range(colcount):
-            max_lens[i] = max(len(headers[i]), len(values(tok)[i]), max_lens[i])
+            max_value_lens[i] = max(len(headers[i]), len(values(tok)[i]), max_value_lens[i])
 
     if not errors_present:
         colcount -= 1
