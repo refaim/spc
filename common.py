c925f43c31fd0a02d10f88b7cd0af227a21f190b
Roman Kharitonov <refaim.vl@gmail.com>
2010-03-18 00:14:27 +1000

    Useless function

--- common.py
+++ common.py
@@ -3,16 +3,11 @@
 def first(seq):
     return seq[0] if seq else None
 
-def last(seq):
-    return seq[len(seq) - 1] if seq and not empty(seq) else None
-
 def second(seq):
     return seq[1] if seq and len(seq) >= 2 else None
 
-def some(seq):
-    for x in seq:
-        if x: return x
-    return None
+def last(seq):
+    return seq[len(seq) - 1] if seq and not empty(seq) else None
 
 def empty(seq):
     return len(seq) == 0
