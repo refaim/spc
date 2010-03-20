0816f6adff3da4669ae26ec70f2e0db1bda3e53c
Roman Kharitonov <refaim.vl@gmail.com>
2010-03-20 12:41:33 +1000

    Obsolete files

--- pychecker.bat
+++
@@ -1,1 +0,0 @@
-C:\Python26\python.exe C:\Python26\Lib\site-packages\pychecker\checker.py %*
--- pychecker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-for entry in $(ls | grep '.py$')
-do
-	if [ $entry != 'enum.py' ]
-	then
-		pychecker $entry | less
-	fi
-done         
