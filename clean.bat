868884f16f8b40963913c8123fc04b0d73195689
Roman Kharitonov <refaim.vl@gmail.com>
2010-08-30 13:23:18 +1100

    Forgot to remove output files

--- clean.bat
+++ clean.bat
@@ -1,5 +1,4 @@
 @echo off
-rd /s /q bin 2>nul >nul
-del /s /q *.pyc 2>nul >nul
-del /q src\version.py 2>nul >nul
-del /q *.gif 2>nul >nul
+rd /s /q bin >nul 2>&1
+del /s /q *.pyc *.o.* >nul 2>&1
+del /q src\version.py >nul 2>&1
