7ea8caf7166b38e672220403f4414205eb64a0bf
Roman Kharitonov <refaim.vl@gmail.com>
2010-09-16 23:42:49 +1100

    Update clean script

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm -rf bin src/version.py
 find . -type f -name "*.pyc" -exec rm -f {} \;
-find . -type f -name "*.o" -exec rm -f {} \;
-find . -type f -name "*.o.*" -exec rm -f {} \;
+find . -type f -name "*.out" -exec rm -f {} \;
+find . -type f -name "*.out.*" -exec rm -f {} \;
