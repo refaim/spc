2745aa3e68b6b8fa558972148d476c1d944dbcf1
Roman Kharitonov <refaim.vl@gmail.com>
2010-10-15 18:27:55 +1100

    Modify some shell scripts

--- clean.sh
+++ clean.sh
@@ -3,6 +3,6 @@ rm -rf bin src/version.py
 find . -type f -name "*.pyc" -exec rm -f {} \;
 find tests -type f -name "*.out" -exec rm -f {} \;
 find tests -type f -name "*.out.*" -exec rm -f {} \;
-find tests -type f -name "*.o" -exec rm -f {} \;
-find tests -type f -name "*.asm" -exec rm -f {} \;
-find tests -type f -name "*.exe" -exec rm -f {} \;
+find tests/gen -type f -name "*.o" -exec rm -f {} \;
+find tests/gen -type f -name "*.asm" -exec rm -f {} \;
+find tests/gen -type f -name "*.exe" -exec rm -f {} \;
