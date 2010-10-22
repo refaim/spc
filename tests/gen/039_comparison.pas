2c42c04d6f39b56aebc1c4ba1311463325570acc
Roman Kharitonov <refaim.vl@gmail.com>
2010-10-22 21:09:47 +1100

    Add test for integer comparison operations generation

---
+++ tests/gen/039_comparison.pas
@@ -0,0 +1,15 @@
+var
+  i: integer;
+begin
+  i := 100;
+  writeln(i = 100);
+  writeln(i > 100);
+  writeln(i < 100);
+  writeln(i >= 100);
+  writeln(i <= 100);
+  
+  writeln(i = 0);
+
+  writeln(i > -20);
+  writeln(i < -20);
+end.
