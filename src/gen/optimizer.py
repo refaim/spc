cdc8aef46e06a51ce137f72349c005ee4d2d7154
Roman Kharitonov <refaim.vl@gmail.com>
2010-10-22 14:51:17 +1100

    Improve low-level optimizer

--- src/gen/optimizer.py
+++ src/gen/optimizer.py
@@ -1,68 +1,196 @@
 # -*- coding: utf-8 -*-
 
+import math
+
 from common.functions import copy_args
 import asm
 
-WINDOW_SIZE = 2
-
 REGISTERS = ('eax', 'ebx', 'ecx', 'edx', 'ebp', 'esi', 'edi')
+
 
 class Optimizer(object):
     @copy_args
     def __init__(self, instructions):
         self.changed = True
-        self.left = 0
+        self.pos = 0
+        self.current_slice_len = -1
+        self.optimizers = [attr for attr in dir(self)
+            if attr.startswith('optimize_')]
 
-    def replace(self, commands, count=WINDOW_SIZE):
+    def is_imm(self, arg):
+        return isinstance(arg, (int, float))
+
+    def is_reg(self, arg):
+        return arg in REGISTERS
+
+    def replace(self, commands):
         if not isinstance(commands, (list, tuple)):
             commands = [commands]
-        self.instructions = self.instructions[:self.left] + list(commands) + self.instructions[self.left+count:]
+        self.instructions = (
+            self.instructions[:self.pos] +
+            list(commands) +
+            self.instructions[self.pos+self.current_slice_len:])
         self.changed = True
 
-    def remove(self, count):
-        del self.instructions[self.left:self.left+count]
+    def remove(self):
+        del self.instructions[self.pos:self.pos+self.current_slice_len]
+        self.changed = True
 
-    def is_command(self, command):
-        current = self.instructions[self.left]
-        return isinstance(current, asm.Command) and command == current.mnem
+    def sequence(self, *commands):
+        
+        def mnem_list(window):
+            result = []
+            for cmd in window:
+                if not isinstance(cmd, asm.Command):
+                    return None
+                cmd = getattr(cmd, 'mnem')
+                if cmd.startswith('set'):
+                    cmd = 'setcc'
+                result.append(cmd)
+            return result
 
-    def is_sequence(self, *commands):
-        window = self.instructions[self.left:self.left+len(commands)]
-        if not all(isinstance(cmd, asm.Command) for cmd in window):
-            return False
-        return list(commands) == [getattr(cmd, 'mnem') for cmd in window]
+        window = self.instructions[self.pos:self.pos+len(commands)]
+        if list(commands) == mnem_list(window):
+            self.current_slice_len = len(commands)
+            return window
+        return None
 
-    def get_slice(self, length):
-        return self.instructions[self.left:self.left+length]
+    def find_near_label(self, name):
+        for label in self.instructions[self.pos+1:]:
+            if not isinstance(label, asm.Label):
+                return None
+            if label.name == name:
+                return True
 
     def optimize(self):
         while self.changed:
             self.changed = False
-            while self.left + WINDOW_SIZE < len(self.instructions):
-                self.optimize_push_pop()
-                self.optimize_mov()
-                self.left += 1
-            self.left = 0
+            while self.pos < len(self.instructions):
+                for func in self.optimizers:
+                    getattr(self, func)()
+                self.pos += 1
+            self.pos = 0
         return self.instructions
 
     def optimize_push_pop(self):
-        if self.is_sequence('push', 'push', 'pop', 'pop'):
-            c = self.get_slice(4)
+        '''
+        push a
+        push b
+        pop c
+        pop d
+        ------->
+        mov d, a                                                               
+        mov c, b
+        '''
+        slc = self.sequence('push', 'push', 'pop', 'pop')
+        if slc:
             self.replace((
-                asm.Command('mov', c[3].arg, c[0].arg),
-                asm.Command('mov', c[2].arg, c[1].arg),
-            ), 4)
+                asm.Command('mov', slc[3].arg, slc[0].arg),
+                asm.Command('mov', slc[2].arg, slc[1].arg),
+            ))
 
-        if self.is_sequence('push', 'pop'):
-            c = self.get_slice(2)
-            self.replace(asm.Command('mov', c[1].arg, c[0].arg))
+        '''
+        push reg/mem
+        pop reg/mem
+        -->
+        mov reg/mem, reg/mem
+        '''
+        slc = self.sequence('push', 'pop')
+        if slc:
+            self.replace(asm.Command('mov', slc[1].arg, slc[0].arg))
+
+    def optimize_arithmetic(self):
+
+        # 'sub reg/mem, 1' --> 'dec reg/mem'
+        slc = self.sequence('sub')
+        if slc and slc[0].args[1] == 1:
+            self.replace(asm.Command('dec', slc[0].args[0]))
+        
+        # 'add reg/mem, 1' --> 'inc reg/mem'
+        slc = self.sequence('add')
+        if slc and slc[0].args[1] == 1:
+            self.replace(asm.Command('inc', slc[0].args[0]))
+
+        def is_power(arg):
+            log = math.log(arg, 2)
+            if log == int(log):
+                return int(log)
+
+        # 'imul reg, 2^n' --> 'shl eax, n'
+        slc = self.sequence('imul')
+        if slc and len(slc[0].args) > 1 and self.is_imm(slc[0].args[1]):
+            value = is_power(slc[0].args[1])
+            if value:
+                self.replace(asm.Command('shl', slc[0].args[0], value))
+
+    def optimize_add_mov(self):
+        slc = self.sequence('add', 'mov')
+        if not slc:
+            return
+
+        if (slc[0].args[0] == slc[1].args[1] and
+            slc[0].args[1] == slc[1].args[0] and
+            not self.is_imm(slc[0].args[1])
+        ):
+            self.replace(asm.Command('add', slc[0].args[1], slc[0].args[0]))
 
     def optimize_mov(self):
-        if self.is_command('mov'):
-            c = self.get_slice(1)[0]
+        '''
+        mov a, imm
+        mov reg/mem, a
+        -->
+        mov reg/mem, imm
+        '''
+        slc = self.sequence('mov', 'mov')
+        if (slc and 
+            slc[0].args[0] == slc[1].args[1] and
+            self.is_imm(slc[0].args[1])
+        ):
+            self.replace(asm.Command('mov', slc[1].args[0], slc[0].args[1]))
+            
 
-            if c.args[0] in REGISTERS and c.args[1] == 0:
-                self.replace(asm.Command('xor', c.args[0], c.args[0]), 1)
+        slc = self.sequence('mov')
+        if not slc:
+            return
 
-            if c.args[0] == c.args[1]:
-                self.remove(1)
+        # 'mov reg, 0' --> 'xor reg, reg'
+        if self.is_reg(slc[0].args[0]) and slc[0].args[1] == 0:
+            self.replace(asm.Command('xor', slc[0].args[0], slc[0].args[0]))
+
+        # remove 'mov reg/mem, reg/mem'
+        elif slc[0].args[0] == slc[0].args[1]:
+            self.remove()
+
+    def optimize_jmp(self):
+        '''
+        jmp label
+        (labels)*
+        label:
+        -->
+        (labels)*
+        label:
+        '''
+        slc = self.sequence('jmp')
+        if slc and self.find_near_label(slc[0].arg):
+            self.remove()
+
+    def optimize_loop(self):
+        '''
+        setcc reg8
+        movzx reg16, reg8
+        test reg16, reg16
+        -->
+        setcc reg8
+        test reg8, reg8
+        '''
+        slc = self.sequence('setcc', 'movzx', 'test')
+        if not slc:
+            return
+
+        if (slc[0].arg == slc[1].args[1] and
+            slc[1].args[0] == slc[2].args[0] == slc[2].args[1]
+        ):
+            self.replace((
+                slc[0],
+                asm.Command('test', slc[0].arg, slc[0].arg),
+            ))
